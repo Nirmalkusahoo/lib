5a627ecb6b04e30255906815e66643cee9deb4f4
i537736 <nirmal.kumar.sahoo@sap.com>
2023-03-30 12:45:51 +0530

    lib

--- fesm2015/sap-domain-viewer-template.mjs
+++ fesm2015/sap-domain-viewer-template.mjs
@@ -14,7 +14,7 @@ class ParentComponent {
         this.route.params.subscribe((params) => {
             this.artifactDetailsService
                 // .getArtifactDetails(params['id'], ArtifactType.Event)
-                .getArtifactDetails(params['id'], 'Solution3') // artifactType should be added from  ArtifactType enum ex: ArtifactType.Event
+                .getArtifactDetails(params['id'], 'solutionapp') // artifactType should be added from  ArtifactType enum ex: ArtifactType.Event
                 .subscribe((data) => {
                 if (data) {
                     this.headerBannerInput.title = data.DisplayName;
@@ -30,9 +30,9 @@ class ParentComponent {
     ngOnInit() {
     }
 }
-ParentComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: ParentComponent, deps: [{ token: i1.ArtifactDetailsService }, { token: i2.ActivatedRoute }], target: i0.ɵɵFactoryTarget.Component });
-ParentComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "14.2.12", type: ParentComponent, selector: "lib-parent", ngImport: i0, template: "<lib-dts-common-header-banner\n  [input]=\"headerBannerInput\"\n></lib-dts-common-header-banner>\n<router-outlet></router-outlet>\n", styles: [""], dependencies: [{ kind: "component", type: i1.CommonHeaderBannerComponent, selector: "lib-dts-common-header-banner", inputs: ["input"], outputs: ["callFunction"] }, { kind: "directive", type: i2.RouterOutlet, selector: "router-outlet", outputs: ["activate", "deactivate", "attach", "detach"], exportAs: ["outlet"] }] });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: ParentComponent, decorators: [{
+ParentComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: ParentComponent, deps: [{ token: i1.ArtifactDetailsService }, { token: i2.ActivatedRoute }], target: i0.ɵɵFactoryTarget.Component });
+ParentComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "14.3.0", type: ParentComponent, selector: "lib-parent", ngImport: i0, template: "<lib-dts-common-header-banner\n  [input]=\"headerBannerInput\"\n></lib-dts-common-header-banner>\n<router-outlet></router-outlet>\n", styles: [""], dependencies: [{ kind: "component", type: i1.CommonHeaderBannerComponent, selector: "lib-dts-common-header-banner", inputs: ["input"], outputs: ["callFunction"] }, { kind: "directive", type: i2.RouterOutlet, selector: "router-outlet", outputs: ["activate", "deactivate", "attach", "detach"], exportAs: ["outlet"] }] });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: ParentComponent, decorators: [{
             type: Component,
             args: [{ selector: 'lib-parent', template: "<lib-dts-common-header-banner\n  [input]=\"headerBannerInput\"\n></lib-dts-common-header-banner>\n<router-outlet></router-outlet>\n" }]
         }], ctorParameters: function () { return [{ type: i1.ArtifactDetailsService }, { type: i2.ActivatedRoute }]; } });
@@ -42,9 +42,9 @@ class ContentComponent {
     ngOnInit() {
     }
 }
-ContentComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: ContentComponent, deps: [], target: i0.ɵɵFactoryTarget.Component });
-ContentComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "14.2.12", type: ContentComponent, selector: "lib-content", ngImport: i0, template: "<div>\n  <h1>Placeholder for content</h1>\n</div>\n", styles: ["div{display:flex;justify-content:center;align-items:center}\n"] });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: ContentComponent, decorators: [{
+ContentComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: ContentComponent, deps: [], target: i0.ɵɵFactoryTarget.Component });
+ContentComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "14.3.0", type: ContentComponent, selector: "lib-content", ngImport: i0, template: "<div>\n  <h1>Placeholder for content</h1>\n</div>\n", styles: ["div{display:flex;justify-content:center;align-items:center}\n"] });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: ContentComponent, decorators: [{
             type: Component,
             args: [{ selector: 'lib-content', template: "<div>\n  <h1>Placeholder for content</h1>\n</div>\n", styles: ["div{display:flex;justify-content:center;align-items:center}\n"] }]
         }], ctorParameters: function () { return []; } });
@@ -61,10 +61,10 @@ const routes = [{
     }];
 class MyLibRouterModule {
 }
-MyLibRouterModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: MyLibRouterModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
-MyLibRouterModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "14.2.12", ngImport: i0, type: MyLibRouterModule, imports: [CommonModule, i2.RouterModule] });
-MyLibRouterModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: MyLibRouterModule, imports: [CommonModule, RouterModule.forChild(routes)] });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: MyLibRouterModule, decorators: [{
+MyLibRouterModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: MyLibRouterModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+MyLibRouterModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "14.3.0", ngImport: i0, type: MyLibRouterModule, imports: [CommonModule, i2.RouterModule] });
+MyLibRouterModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: MyLibRouterModule, imports: [CommonModule, RouterModule.forChild(routes)] });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: MyLibRouterModule, decorators: [{
             type: NgModule,
             args: [{
                     imports: [CommonModule, RouterModule.forChild(routes)]
@@ -101,20 +101,20 @@ class LoadedFromRouterComponent {
         });
     }
 }
-LoadedFromRouterComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: LoadedFromRouterComponent, deps: [{ token: i1.ArtifactDetailsService }, { token: i2.ActivatedRoute }], target: i0.ɵɵFactoryTarget.Component });
-LoadedFromRouterComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "14.2.12", type: LoadedFromRouterComponent, selector: "lib-loaded-from-router", ngImport: i0, template: "<!--<h1>Lazily loaded component using Router</h1>-->\n<lib-dts-common-header-banner\n  [input]=\"headerBannerInput\"\n></lib-dts-common-header-banner>\n", styles: [""], dependencies: [{ kind: "component", type: i1.CommonHeaderBannerComponent, selector: "lib-dts-common-header-banner", inputs: ["input"], outputs: ["callFunction"] }] });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: LoadedFromRouterComponent, decorators: [{
+LoadedFromRouterComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: LoadedFromRouterComponent, deps: [{ token: i1.ArtifactDetailsService }, { token: i2.ActivatedRoute }], target: i0.ɵɵFactoryTarget.Component });
+LoadedFromRouterComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "14.3.0", type: LoadedFromRouterComponent, selector: "lib-loaded-from-router", ngImport: i0, template: "<!--<h1>Lazily loaded component using Router</h1>-->\n<lib-dts-common-header-banner\n  [input]=\"headerBannerInput\"\n></lib-dts-common-header-banner>\n", styles: [""], dependencies: [{ kind: "component", type: i1.CommonHeaderBannerComponent, selector: "lib-dts-common-header-banner", inputs: ["input"], outputs: ["callFunction"] }] });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: LoadedFromRouterComponent, decorators: [{
             type: Component,
             args: [{ selector: 'lib-loaded-from-router', template: "<!--<h1>Lazily loaded component using Router</h1>-->\n<lib-dts-common-header-banner\n  [input]=\"headerBannerInput\"\n></lib-dts-common-header-banner>\n" }]
         }], ctorParameters: function () { return [{ type: i1.ArtifactDetailsService }, { type: i2.ActivatedRoute }]; } });
 
 class MyLibModule {
 }
-MyLibModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: MyLibModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
-MyLibModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "14.2.12", ngImport: i0, type: MyLibModule, declarations: [LoadedFromRouterComponent,
+MyLibModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: MyLibModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+MyLibModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "14.3.0", ngImport: i0, type: MyLibModule, declarations: [LoadedFromRouterComponent,
         ParentComponent], imports: [MyLibRouterModule, FalconUiPedestalModule, RouterModule] });
-MyLibModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: MyLibModule, imports: [MyLibRouterModule, FalconUiPedestalModule, RouterModule] });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: MyLibModule, decorators: [{
+MyLibModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: MyLibModule, imports: [MyLibRouterModule, FalconUiPedestalModule, RouterModule] });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: MyLibModule, decorators: [{
             type: NgModule,
             args: [{
                     declarations: [
@@ -132,9 +132,9 @@ class StandAloneComponent {
     ngOnInit() {
     }
 }
-StandAloneComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: StandAloneComponent, deps: [], target: i0.ɵɵFactoryTarget.Component });
-StandAloneComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "14.2.12", type: StandAloneComponent, isStandalone: true, selector: "lib-stand-alone", ngImport: i0, template: "<h1>Lazily loaded standalone component</h1>\n", styles: [""] });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.12", ngImport: i0, type: StandAloneComponent, decorators: [{
+StandAloneComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: StandAloneComponent, deps: [], target: i0.ɵɵFactoryTarget.Component });
+StandAloneComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "14.3.0", type: StandAloneComponent, isStandalone: true, selector: "lib-stand-alone", ngImport: i0, template: "<h1>Lazily loaded standalone component</h1>\n", styles: [""] });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.3.0", ngImport: i0, type: StandAloneComponent, decorators: [{
             type: Component,
             args: [{ standalone: true, selector: 'lib-stand-alone', template: "<h1>Lazily loaded standalone component</h1>\n" }]
         }], ctorParameters: function () { return []; } });
@@ -148,4 +148,4 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.12", ngImpo
  */
 
 export { MyLibModule, StandAloneComponent };
-//# sourceMappingURL=my-lib.mjs.map
+//# sourceMappingURL=sap-domain-viewer-template.mjs.map
